d432a72b61a020b127dccc1476f36c0843f19db3
Aye Guzman <ayelenguzman18@gmail.com>
2025-11-07 10:55:50 -0300

    terminado, no cambiar el rosa

--- target/classes/scheme.sql
+++ target/classes/scheme.sql
@@ -6,4 +6,23 @@ CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT, -- Clave primaria autoincremental para SQLite
     name TEXT NOT NULL UNIQUE,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
     password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
+<<<<<<< HEAD
 );
+=======
+);
+
+CREATE TABLE persona (
+    dni INTEGER PRIMARY KEY,
+    realName TEXT NOT NULL,
+    surname TEXT NOT NULL
+);
+
+CREATE TABLE docente (
+    dni INTEGER PRIMARY KEY,
+    departament TEXT NOT NULL,
+    correo TEXT NOT NULL,
+    FOREIGN KEY (dni) REFERENCES persona(dni)
+     ON DELETE CASCADE
+     ON UPDATE CASCADE
+);
+>>>>>>> c1368f1 (terminado, no le cambien el rosaaa)
