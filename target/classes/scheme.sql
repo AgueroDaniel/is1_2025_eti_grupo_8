5c46991d827f41469a824781c93f98f78ac64996
Aye Guzman <ayelenguzman18@gmail.com>
2025-11-07 11:18:24 -0300

    ahora si version finaal, nose olviden de crear las tablas para que ande

--- target/classes/scheme.sql
+++ target/classes/scheme.sql
@@ -1,22 +1,18 @@
--- Elimina la tabla 'users' si ya existe para asegurar un inicio limpio
+
+ -- Elimina la tabla 'users' si ya existe para asegurar un inicio limpio
 DROP TABLE IF EXISTS users;
 
--- Crea la tabla 'users' con los campos originales, adaptados para SQLite
+ -- Crea la tabla 'users' con los campos originales, adaptados para SQLite
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT, -- Clave primaria autoincremental para SQLite
     name TEXT NOT NULL UNIQUE,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
     password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
-<<<<<<< HEAD
 );
-=======
-);
-
 CREATE TABLE persona (
     dni INTEGER PRIMARY KEY,
     realName TEXT NOT NULL,
     surname TEXT NOT NULL
 );
-
 CREATE TABLE docente (
     dni INTEGER PRIMARY KEY,
     departament TEXT NOT NULL,
@@ -25,4 +21,3 @@ CREATE TABLE docente (
      ON DELETE CASCADE
      ON UPDATE CASCADE
 );
->>>>>>> c1368f1 (terminado, no le cambien el rosaaa)
